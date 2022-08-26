14c9ebc71373b650bf1b6e04ca6ee9075d5b3e15
gimminjae <min356812@naver.com>
2022-08-26 13:58:03 +0900

    세팅

---
+++ src/main/java/com/example/qnaquerydsl/QnAQueryDslApplication.java
@@ -0,0 +1,13 @@
+package com.example.qnaquerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnAQueryDslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QnAQueryDslApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/qnaquerydsl/QnAQueryDslApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qnaquerydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnAQueryDslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
