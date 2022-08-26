214375a306e381461e060b412b9de7d6baae24b1
gimminjae <min356812@naver.com>
2022-08-26 14:15:14 +0900

    jpaQueryFactory bean 추가

--- src/main/java/com/example/qnaquerydsl/user/repository/UserRepositoryCustom.java
+++ src/main/java/com/example/qnaquerydsl/user/repository/UserRepositoryCustom.java
@@ -1,4 +1,7 @@
 package com.example.qnaquerydsl.user.repository;
 
+import com.example.qnaquerydsl.user.entity.SiteUser;
+
 public interface UserRepositoryCustom {
+    SiteUser getQslUser(Long id);
 }
